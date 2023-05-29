91fb9f6e787a0ec04362e05f74f062b2024ad93e
Iris <134139646+VerwegenIV@users.noreply.github.com>
2023-05-30 00:15:27 +0200

    Updated independent set
    Added solver

--- IndepSet.py
+++ IndepSet.py
@@ -38,27 +38,34 @@ def find_odd_cycle(n, vardict):
                     vardict[var2]['r']:
                 G.add_edge(var1[4:-1], var2[4:-1])
     if vardict[dict_keys[-1]]['r'] == 0:
-        vars_in_0.append(dict_keys[-1])
+        vars_in_0.append(dict_keys[-1][4:-1])
+    if len(vars_in_0) < 2:
+        return odd_cycles
     for node in vars_in_0:
         v_in_0 = vars_in_0.copy()
         v_in_0.remove(node)
         H = nx.Graph(G)
         H.remove_nodes_from(v_in_0)
-        cycles = cycle_sub(n, node, 'a', [node], H, {})
+        print(H.edges)
+        cycles = cycle_sub(n, node, 'a', [node], H, set())
         odd_cycles.extend(cycles)
-    print(odd_cycles)
+    return odd_cycles
 
 
 def cycle_sub(n, node, prev_node, current_c, G, cycles):
-    if len(current_c) == n - 1 and current_c[0] in list(G.adj[prev_node]):
-        cycles.add(current_c)
+    print(node, prev_node, current_c)
+    if len(current_c) == n - 1 and current_c[0] in list(G.adj[node]):
+        cycles.add(frozenset(current_c))
         return cycles
     c_c = current_c.copy()
     if len(c_c) > 1:
         c_c.remove(prev_node)
     if len(set(c_c).intersection(set(G.adj[node]))) > 0:
         return cycles
-    for new_node in list(G.adj[node]):
+    adj_list = list(G.adj[node])
+    if prev_node != 'a':
+        adj_list.remove(prev_node)
+    for new_node in adj_list:
         H = nx.Graph(G)
         current_c.append(new_node)
         for other_node in list(G.adj[new_node]):
@@ -69,10 +76,45 @@ def cycle_sub(n, node, prev_node, current_c, G, cycles):
     return cycles
 
 
+def solve_inst(model, n, edges):
+    print('Solving')
+    old_triangles = []
+    old_cycles = set()
+    integer = 0
+    while not integer:
+        vardict, posvar = SmallCG.construct_vardict(model, edges)
+        triangles = find_triangles(vardict)
+        odd_cycles = find_odd_cycle(n, vardict)
+        if triangles == old_triangles or old_cycles == odd_cycles:
+            return False
+        old_triangles = triangles.copy()
+        old_cycles = odd_cycles.copy()
+        print('found triangles', triangles)
+        print('found cycles', odd_cycles)
+        if not triangles and not odd_cycles:
+            return False
+        for triangle in triangles:
+            for var in triangle:
+                print(var, model.getVarByName(var))
+            model.addConstr((quicksum(model.getVarByName(var) for var in triangle) <= 1), 't')
+        for cycle in odd_cycles:
+            model.addConstr((quicksum(model.getVarByName("_var" + var + "_") for var in cycle) <= (n - 2) / 2), 'c')
+        model.update()
+        model.optimize()
+        for var in model.getVars():
+            if var.x > 0.001:
+                print(var)
+        integer = 1
+        for var in model.getVars():
+            if 0.001 < var.x < 0.999:
+                integer = 0
+    return True
+
+
 def solve_indep_set():
     # list with models that have an initially fractional solution
     number_viol = []
-    # list with models that have an initially integer solution
+    # list with models that have an initially integral solution
     int_sol = []
     # list with models that have an initially fractional solution, but no violated inequalities
     no_found = []
@@ -82,9 +124,9 @@ def solve_indep_set():
     # Iterate over all possible test cases
     # for s in ['06', '12', '18']:
     for s in ['06']:
-        for p in range(5, 6):
-            for k in range(32,33):
-                print("DIT IS K,S,P", k, s, p)
+        for p in range(5, 10):
+            for k in range(50):
+                print("DIT IS K, S, P", k, s, p)
                 if k < 10:
                     file = 'bin0' + s + '_0' + str(p) + '0_00' + str(k) + '.srr.lp'
                 else:
@@ -112,11 +154,14 @@ def solve_indep_set():
                     int_sol.append((s, p, k))
                 else:
                     # Find small independent set violations
-                    # find_triangles(vardict)
-                    find_odd_cycle(n, vardict)
-                    # if violated:
-                    #     number_viol.append((s, p, k))
-                    #     nrof_viol[(s, p, k)] = {'nrof_viol': len(violated), 'nrof_iter': nrof_iter, 'objectives': objs}
-                    # else:
-                    #     no_found.append((s, p, k))
-    # return len(number_viol), len(int_sol), no_found, nrof_viol
+                    triangles = find_triangles(vardict)
+                    odd_cycles = find_odd_cycle(n, vardict)
+                    # Try to solve the instance
+                    solved = solve_inst(model, n, edges)
+                    if triangles or odd_cycles:
+                        number_viol.append((s, p, k))
+                        nrof_viol[(s, p, k)] = {'nrof_triangles': len(triangles), 'nrof_cycles': len(odd_cycles),
+                                                'solved': solved}
+                    else:
+                        no_found.append((s, p, k))
+    return len(number_viol), len(int_sol), no_found, nrof_viol
